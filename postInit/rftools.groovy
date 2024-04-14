66698d7e8cb37f662679b7b1fd2e7109a65554d4
bioastroiner <68257484+bioastroiner@users.noreply.github.com>
2024-04-14 15:38:27 +0330

    + rftools

--- postInit/rftools.groovy
+++ postInit/rftools.groovy
@@ -1,19 +1,33 @@
 // MOD_LOADED rftools
-crafting.removeByOutput(item('rftools:machineFrame'))
-crafting.removeByOutput(item('rftools:machineBase'))
-crafting.removeByOutput(item('rftools:storageScannerBlock'))
-crafting.removeByOutput(item('rftools:dimensionBuilderBlock'))
-
-crafting.addShaped(item('rftools:machineFrame') * 2,[
-    [ore('plateElectrum'),ore('plateElectrum'),ore('plateElectrum')]
-    [ore('plateElectrum'),ore('machineBlockBasic'),ore('plateElectrum')]
-    [ore('plateElectrum'),ore('plateElectrum'),ore('plateElectrum')]
+crafting.removeByOutput(item('rftools:machine_frame'))
+crafting.removeByOutput(item('rftools:machine_base'))
+crafting.removeByOutput(item('rftools:storage_scanner'))
+crafting.removeByOutput(item('rftoolsdim:dimension_builder'))
+// machine blocks
+crafting.shapedBuilder()
+    .key('e',ore('plateElectrum'))
+    .key('m',ore('machineBlockBasic'))
+    .row(' e ')
+    .row('eme')
+    .row(' e ')
+    .output(item('rftools:machine_frame'))
+    .register()
+// crafting.addShaped(item('rftools:machine_frame') * 1,[
+//     [null,ore('plateElectrum'),null]
+//     [ore('plateElectrum'),ore('machineBlockBasic'),ore('plateElectrum')]
+//     [null,ore('plateElectrum'),null]
+// ])
+crafting.addShapeless(item('rftools:machine_base') * 2,[
+    ore('craftingToolSaw'),item('rftools:machine_frame')
 ])
-crafting.addShapeless(item('rftools:machineBase') * 4,[
-    ore('craftingToolSaw'),item('rftools:machineFrame')
-])
-crafting.addShaped(item('rftools:storageScannerBlock'),[
+// storage
+crafting.addShaped(item('rftools:storage_scanner'),[
     [ore('plateStainlessSteel'),ore('plateStainlessSteel'),ore('plateStainlessSteel')],
-    [ore('circuitAdvanced'),item('rftools:machineFrame'),ore('circuitAdvanced')]
+    [ore('circuitAdvanced'),item('rftools:machine_frame'),ore('circuitAdvanced')],
     [ore('plateStainlessSteel'),ore('plateStainlessSteel'),ore('plateStainlessSteel')]
+])
+crafting.addShaped(item('rftoolsdim:dimension_builder'),[
+    [ore('plateTungstensteel'),ore('circuitUltimate'),ore('plateTungstensteel')],
+    [item('rftools:machine_frame'),ore('machineBlockElite'),item('gtclassic:idsu')],
+    [ore('plateTungstensteel'),ore('circuitUltimate'),ore('plateTungstensteel')]
 ])
